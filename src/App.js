6131b96de7c7ac123e6aa76a2df540b4dae81290
AkasgVarma007 <g.akashvarma@gmail.com>
2021-04-08 13:23:05 +0530

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import './App.css';
 import ScoreBoard from './components/ScoreBoard'
 import { useState, useEffect } from "react";
 import $ from 'jquery';
+import useEventListener from './hooks/useEventListner';
+let gbLeft;
+let gbTop;
+
 function App() {
   const [gameState, setGameState] = useState('initial');
   const [ballCord, setBallCord] = useState({ top: null, left: null })
   const [leftPongCord, setLeftPongCord] = useState({ top: '0px' })
   const [rightPongCord, setRightPongCord] = useState({ top: "0px" })
-  $(document).on('keypress', (e) => {
-    if (e.key === 'w') {
-      console.log(e);
-      let updatedCord = parseInt(leftPongCord.top.slice(0, -2)) + 20;
-      setLeftPongCord({ top: updatedCord + 'px' });
-    }
-  })
   let game = () => {
     if (gameState == 'initial') {
       return (
@@ -33,19 +30,54 @@ function App() {
       )
     }
   }
+  let pongMove = ({ key }) => {
+    if (String(key) === 's') {
+      if (parseInt(leftPongCord.top.slice(0, -2)) < gbTop) {
+        let updatedCord = parseInt(leftPongCord.top.slice(0, -2)) + 10;
+        setLeftPongCord({ top: (updatedCord + 'px') });
+      }
+    }
+    else if (String(key) === 'w') {
+      if (parseInt(leftPongCord.top.slice(0, -2))  > 0) {
+        let updatedCord = parseInt(leftPongCord.top.slice(0, -2)) - 10;
+        setLeftPongCord({ top: (updatedCord + 'px') });
+      }
+    }
+    else if (String(key) === 'ArrowUp') {
+      if (parseInt(rightPongCord.top.slice(0, -2))  < gbTop) {
+        let updatedCord = parseInt(rightPongCord.top.slice(0, -2)) + 10;
+        setRightPongCord({ top: (updatedCord + 'px') });
+        console.log(String(key));
+      }
+    }
+    else if (String(key) === 'ArrowDown') {
+      if (parseInt(rightPongCord.top.slice(0, -2))  > 0) {
+        let updatedCord = parseInt(rightPongCord.top.slice(0, -2)) - 10;
+        setRightPongCord({ top: (updatedCord + 'px') });
+        console.log(String(key));
+      }
+    }
+  }
   useEffect(() => {
     $('#divGameBox').on('click', () => {
       if (gameState == 'initial') {
-        setBallCord({
-          left: Math.floor(Math.random() * ($('#divGameBox').width() * 0.7) + ($('#divGameBox').width() * 0.3)) + 'px',
-          top: Math.floor(Math.random() * ($('#divGameBox').height() * 0.8) + ($('#divGameBox').width() * 0.2)) + 'px'
-        })
         setGameState('start')
+        gbTop = $('#divGameBox').height() - 40;
+        gbLeft = $('#divGameBox').width() - 40;
       }
-    })
+    });
   }, []);
   useEffect(() => {
+    if (gameState == "start") {
+      setBallCord({
+        left: Math.floor(Math.random() * (gbLeft * 0.4)) + (gbLeft * 0.3) + 'px',
+        top: Math.floor(Math.random() * (gbTop * 0.6)) + (gbTop * 0.2) + 'px'
+      })
+    }
   }, [gameState])
+
+  useEventListener('keydown', pongMove);
+
   return (
     <div className="App">
       <div id="divGameBox" className="gameBox">
